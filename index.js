cc1edf5415be11156edc305a2ee691084ef3d6b5
Alexandr Y. Burkov <alexburkov93@gmail.com>
2019-11-08 00:16:07 +0300

    Add bootstrap and regiter page

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const exphbs = require('express-handlebars')
 const app = express()
 const http=require("http")
 const bodyParser=require("body-parser");
+const request = require('sync-request');
+
 
 app.engine('.hbs', exphbs({
     defaultLayout: 'main',
@@ -17,11 +19,32 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+function requestToMainServer(method, path) {
+    var res = request(method, "http://localhost:1234" + path);
+    return JSON.parse(res.getBody());
+}
+
 // Главная страница
 app.get('/', (request, response) => {
-	response.render('home')
+    var res = requestToMainServer("GET", "/hi");
+    console.log(res);
+    response.render('home', { params: JSON.stringify(res) });
 });
 
+app.get('/registration', (request, response) => {
+    response.render('registration', {
+        "form_class": "needs-validation"
+    });
+});
+
+app.post('/registration', (request, response) => {
+    params = {
+        form_class: "was-validated",
+        name_error: request.body.name.length > 0 ? "" : "Введите имя"
+    }
+    console.log(params);
+    response.render('registration', params);
+});
 
 console.log("Server started")
 app.listen(3000)
