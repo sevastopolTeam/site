66a996c1004031a018fac0e2bffeb500e0385682
Alexandr Y. Burkov <alexburkov93@gmail.com>
2019-12-14 17:11:27 +0300

    Add login

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 const http=require("http")
 const bodyParser=require("body-parser");
 const request = require('sync-request');
+const fs = require('fs');
 var cookieParser = require('cookie-parser')
 
 app.engine('.hbs', exphbs({
@@ -20,48 +21,29 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser())
 
-function requestToMainServer(method, path, options = {}) {
-    var res = request(method, "http://localhost:1234" + path, { json: options });
-    console.log(res.getBody());
-    console.log(JSON.parse(res.getBody()))
+function requestToMainServer(method, path, data = {}) {
+    console.log(data);
+    var res = request(
+        method, 
+        "http://localhost:1234" + path,
+        { headers: data["Headers"], json: data["Params"] }
+    );
+
     return JSON.parse(res.getBody());
 }
 
-VALIDATION_ERRORS = {
-    "RegistrationPage": {
-        "Name": {
-            "CanNotBeEmpty": "Введите имя"
-        },
-        "Email": {
-            "CanNotBeEmpty": "Введите email",
-            "MustBeEmail": "Введите корректный email",
-            "AlreadyExists": "Пользователь с таким Email уже существует"
-        },
-        "Phone": {
-            "CanNotBeEmpty": "Введите номер телефона",
-            "MustBePhone": "Введите номер телефона в формате +71234567890"
-        },
-        "Password": {
-            "CanNotBeEmpty": "Введите пароль"
-        },
-        "RepeatPassword": {
-            "MustBeSame": "Не совпадает с паролем"
-        }
-    }
+const VALIDATION_ERRORS = {
+    "RU": JSON.parse(fs.readFileSync('content/ru.json', 'utf8'))
 }
 
-function getValidationError(page, field, error) {
-    console.log(page)
-    console.log(field)
-    console.log(error)
-    return VALIDATION_ERRORS[page][field][error];
+function getValidationError(language, page, field, error) {
+    return VALIDATION_ERRORS[language][page]["ValidationError"][field][error];
 }
 
 // Главная страница
 app.get('/', (request, response) => {
     var res = requestToMainServer("GET", "/hi");
-    console.log(res);
-    response.cookie("userData", "testCookiesa");
+    // console.log(res);
     // response.send(JSON.stringify(res));
     response.render('home', { params: JSON.stringify(res) });
 });
@@ -73,27 +55,32 @@ app.get('/registration', (request, response) => {
 
 app.post('/registration', (request, response) => {
     // console.log(request.cookies);
-    var serverResponse = requestToMainServer("POST", "/api/english/registration", {
-        Phone: request.body.Phone,
-        Name: request.body.Name,
-        Email: request.body.Email,
-        Password: request.body.Password,
-        RepeatPassword: request.body.RepeatPassword
-    });
+    var serverResponse = requestToMainServer(
+        "POST",
+        "/api/english/users",
+        {
+            "Params": {
+                Phone: request.body.Phone,
+                Name: request.body.Name,
+                Email: request.body.Email,
+                Password: request.body.Password,
+                RepeatPassword: request.body.RepeatPassword
+            }
+        }
+    );
 
     params = {
         Status: (serverResponse["Status"] == "Ok"),
         Request: request,
         ServerResponse: serverResponse,
         ValidationErrors: {}
-    }
-
-    console.log(serverResponse)
+    };
 
     if (serverResponse["Status"] == "ValidationError") {
         for (var key in serverResponse["ValidationErrors"]) {
             if (serverResponse["ValidationErrors"][key].length > 0) {
                 params["ValidationErrors"][key] = getValidationError(
+                    "RU",
                     "RegistrationPage",
                     key,
                     serverResponse["ValidationErrors"][key][0]
@@ -104,6 +91,70 @@ app.post('/registration', (request, response) => {
     console.log(params["Status"]);
 
     response.render('registration', params);
+});
+
+app.get('/login', (request, response) => {
+    // console.log(request.cookies);
+    response.render('login');
+});
+
+app.post('/login', (request, response) => {
+    // console.log(request.cookies);
+    var serverResponse = requestToMainServer(
+        "POST",
+        "/api/english/login",
+        {
+            "Params": {
+                Email: request.body.Email,
+                Password: request.body.Password
+            }
+        }
+    );
+
+    params = {
+        Status: (serverResponse["Status"] == "Ok"),
+        Request: request,
+        ServerResponse: serverResponse,
+        ValidationErrors: {}
+    }
+
+    if (serverResponse["Status"] == "ValidationError") {
+        for (var key in serverResponse["ValidationErrors"]) {
+            if (serverResponse["ValidationErrors"][key].length > 0) {
+                params["ValidationErrors"][key] = getValidationError(
+                    "RU",
+                    "LoginPage",
+                    key,
+                    serverResponse["ValidationErrors"][key][0]
+                )
+            }
+        }
+    }
+    if (serverResponse["Status"] == "Ok") {
+        response.cookie("SessionToken", serverResponse["Body"]["SessionToken"]);
+    }
+    response.render('login', params);
+});
+
+app.get('/admin/sessions', (request, response) => {
+    var serverResponse = requestToMainServer(
+        "GET",
+        "/api/english/admin/sessions",
+        {
+            "Params": {
+                "Email": request.body.Email,
+                "Password": request.body.Password
+            },
+            "Headers": {
+                "Authorization": request.cookies["SessionToken"]
+            }
+        }
+    );
+    if (serverResponse["Error"] == "AccessDenied") {
+        response.render('access_denied');
+    } else {
+        response.render('admin_sessions', serverResponse);
+    }
 });
 
 console.log("Server started")
