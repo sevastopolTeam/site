fa989377598d7c227a6dacfb8ea5010c8ae59a22
AlexBurkov <buriksurik@mail.ru>
2020-04-02 23:21:23 +0300

    Add translations

--- index.js
+++ index.js
@@ -97,8 +97,8 @@ app.post('/login', (request, response) => {
         "/api/english/login",
         {
             "Params": {
-                Email: request.body.Email,
-                Password: request.body.Password
+                "Email": request.body.Email,
+                "Password": request.body.Password
             }
         }
     );
@@ -144,6 +144,68 @@ app.get('/admin/sessions', (request, response) => {
         response.render('admin_sessions', serverResponse);
     }
 });
+
+app.get('/admin/translations', (request, response) => {
+    var serverResponse = requestToMainServer(
+        "GET",
+        "/api/english/admin/translations",
+        {
+            "Headers": {
+                "Authorization": request.cookies["SessionToken"]
+            }
+        }
+    );
+    if (serverResponse["Error"] == "AccessDenied") {
+        response.render('access_denied');
+    } else {
+        response.render('admin_translations', serverResponse);
+    }
+});
+
+app.get('/admin/add_translation', (request, response) => {
+    response.render('admin_add_translation');
+});
+
+app.post('/admin/add_translation', (request, response) => {
+    var serverResponse = requestToMainServer(
+        "POST",
+        "/api/english/admin/translations",
+        {
+            "Params": {
+                "ValueFrom": request.body.ValueFrom,
+                "ValueTo": request.body.ValueTo,
+                "LanguageFrom": request.body.LanguageFrom,
+                "LanguageTo": request.body.LanguageTo,
+                "OriginUrl": request.body.OriginUrl
+            },
+            "Headers": {
+                "Authorization": request.cookies["SessionToken"]
+            }
+        }
+    );
+
+    params = {
+        Status: (serverResponse["Status"] == "Ok"),
+        Request: request,
+        ServerResponse: serverResponse,
+        ValidationErrors: {}
+    }
+
+    if (serverResponse["Status"] == "ValidationError") {
+        for (var key in serverResponse["ValidationErrors"]) {
+            if (serverResponse["ValidationErrors"][key].length > 0) {
+                params["ValidationErrors"][key] = getValidationError(
+                    "RU",
+                    "AddTranslationPage",
+                    key,
+                    serverResponse["ValidationErrors"][key][0]
+                )
+            }
+        }
+    }
+    response.render('admin_add_translation', params);
+});
+
 
 console.log("Server started")
 app.listen(3000)
